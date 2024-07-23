da58a43b4d757bd1a95a6c190e9aab8e3900f320
Birgit Pauli-Haack <birgit.pauli@gmail.com>
2024-07-23 17:01:24 +0200

    vresion bump

--- gutenberg.php
+++ gutenberg.php
@@ -6,7 +6,7 @@
 
  * Requires at least: 6.4
  * Requires PHP: 7.4
- * Version: 19.0.20240722
+ * Version: 19.0.20240723
  * Author: Gutenberg Team and Birgit Pauli-Haack (Gutenberg Times)
  * Text Domain: gutenberg
  * GitHub Plugin URI: bph/gutenberg
