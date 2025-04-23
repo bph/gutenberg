42ea3182e985df3ba934b7203be2a70ef40939d7
Birgit Pauli-Haack <birgit.pauli@gmail.com>
2025-04-23 10:23:45 +0200

    version + tag update

--- gutenberg.php
+++ gutenberg.php
@@ -4,9 +4,9 @@
  * Plugin URI: https://github.com/bph/gutenberg
  * Description: Zip from trunk (the default branch) of WordPress Gutenberg GitHub repo: Gutenberg This is the development plugin for the new block editor in core. If you have trouble seeing updates, go to <em>Git Updater > Settings</em>  and use the button <em>Refresh Cache</em>. Ping @bph on WPSlack for questions. Or <a href="https://github.com/bph/gutenberg/discussions/new">post on the Discussion board</a>.
 
- * Requires at least: 6.5
+ * Requires at least: 6.6
  * Requires PHP: 7.4
- * Version: 20.9.20250417
+ * Version: 21.0.20250423
  * Author: Gutenberg Team and Birgit Pauli-Haack (Gutenberg Times)
  * Text Domain: gutenberg
  * GitHub Plugin URI: bph/gutenberg
