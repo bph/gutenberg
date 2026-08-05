4765f18f85eed7e1250148a79a8d230928b1c4d5
Andy Fragen <andy@thefragens.com>
2026-08-05 18:16:37 -0400

    add back GU integration
    add GUL integration with guard
    
    return empty line
    
    remove extra line
    
    Revert "remove extra line"
    
    This reverts commit 0aaf3e8b7af900bac271c8539a3c4b28ae736c7f.
    
    put all those extra spaces back

--- gutenberg.php
+++ gutenberg.php
@@ -10,17 +10,45 @@
  * Author: Gutenberg Team and Birgit Pauli-Haack (Gutenberg Times)
  * Text Domain: gutenberg
  * Update URI: https://gutenbergtimes.com
+ * GitHub Plugin URI: bph/gutenberg
+ * Primary Branch: trunk
+ * Release Asset: true
  *
  * @package gutenberg
  */
 
+// Add the development channel to the Git Updater Lite API URL for the Gutenberg plugin.
+add_filter( 'git_updater_lite_api_url', fn( $url, $slug ) => ( 'gutenberg' === $slug ) ? add_query_arg( [ 'channel' => 'development' ], $url ) : $url, 10, 2 );
+
  // Initialize Git Updater Lite.
- require_once __DIR__ . '/vendor/afragen/git-updater-lite/Lite.php';
-( new \Fragen\Git_Updater\Lite( __FILE__ ) )->run();
+ if ( file_exists( __DIR__ . '/vendor/afragen/git-updater-lite/Lite.php' ) ) {
+	require_once __DIR__ . '/vendor/afragen/git-updater-lite/Lite.php';
+	( new \Fragen\Git_Updater\Lite( __FILE__ ) )->run();
+}
 
-// Get dev release assets for Gutenberg plugin.
-add_filter( 'gu_dev_release_asset', fn( $bool, $repo ) => ( 'gutenberg' === $repo->slug ), 10, 2 );
 
+// GitHub Updater filters.
+add_filter(
+	'gu_override_dot_org',
+	function ( $overrides ) {
+		return array_merge(
+			$overrides,
+			array( 'gutenberg/gutenberg.php' )
+		);
+	}
+);
+add_filter(
+	'gu_release_asset_rollback',
+	function ( $rollback, $file ) {
+		if ( $file === plugin_basename( __FILE__ ) ) {
+			return [ 'gutenberg-nightly' ];
+		}
+	},
+	10,
+	2
+);
+add_filter( 'gu_no_release_asset_branches', '__return_true' );
+// End GitHub Updater filters.
 
 
 ### BEGIN AUTO-GENERATED DEFINES
